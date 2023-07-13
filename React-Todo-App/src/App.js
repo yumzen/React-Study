cd433c82623257b8fe78b8024abedfeff512d45f
yumzen <yeom.ji.eun35@gmail.com>
2023-07-13 10:09:01 +0900

    Create: 할일 추가하기

--- React-Todo-App/src/App.js
+++ React-Todo-App/src/App.js
@@ -2,12 +2,59 @@ import "./App.css"
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
+import {useState,useRef} from "react";
+
+//mock(목) 데이터란 모조품 데이터로, 기능을 완벽히 구현하지 않은 상태에서 테스트를 목적으로 사용하는 데이터
+const mockTodo = [
+  {
+    id : 0,
+    isDone : false,
+    content : "React 공부하기",
+    createdDate : new Date().getTime(),
+  },
+  {
+    id : 1,
+    isDone : false,
+    content : "빨래 널기",
+    createdDate : new Date().getTime(),
+  },
+  {
+    id : 2,
+    isDone : false,
+    content : "노래 연습하기",
+    createdDate : new Date().getTime(),
+  },
+]
 
 function App() {
+  /*
+  useState를 이용해 할 일 아이템의 상태를 관리할 State를 만든다. 
+  함수 useState에서 인수로 빈 배열을 전달해 State 변수 todo의 기본값을 빈 배열로 초기화한다.
+  즉, State 변수 todo는 데이터를 저장하는 배열이면서 일종의 DB 역할을 수행
+  */
+  const[todo, setTodo] = useState(mockTodo);
+
+  //초기값이 3인 Ref 객체를 생성해 idRef에 저장
+  const idRef = useRef(3);
+
+  //새 할 일 아이템을 추가하는 함수 onCreate
+  const onCreate = (content) => {
+    const newItem = {
+      id : idRef.current,
+      content,
+      isDone : false,
+      createdDate : new Date().getTime(),
+    };
+    
+    // 배열의 스프레드 연산자를 활용해 newItem을 포함한 새 배열을 만들어 State 변수 todo를 업데이트-> 새롭게 추가된 아이템은 항상 배열의 0번 요소가 됨
+    setTodo([newItem, ...todo]);
+    idRef.current +=1;
+  }
+
   return (
     <div className = "App">
       <Header/>
-      <TodoEditor/>
+      <TodoEditor onCreate = {onCreate}/>
       <TodoList/>
     </div>
   );
--- React-Todo-App/src/component/TodoEditor.js
+++ React-Todo-App/src/component/TodoEditor.js
@@ -1,15 +1,64 @@
+import { useState, useRef } from "react";
 import "./TodoEditor.css";
 
-const TodoEditor = () => {
+//Props 객체를 구조 분해 할당
+const TodoEditor = ({onCreate}) => {
+    const [content,setContent] = useState("");
+    //입력 폼의 onChange 이벤트 핸들러 onChangeContent 생성
+    const OnchangeContent = (e) => {
+        setContent(e.target.value);
+    }
+
+    const inputRef = useRef();
+
+    //추가 버튼에 대한 이벤트 핸들러 onSubmit 생성
+    //onSubmit은 함수 onCreate를 호출하고ㅓ 인수로 content의 값을 전달
+    const onSubmit = () => {
+        if (!content){
+            /* 빈 입력 방지를 위해 할 일 입력 폼을 제어할 객체 inputRef 생성
+            빈 문자열이면 inputRef가 current(현재값)로 저장한 요소에 포커스하고 종료
+            입력 폼에 커서가 위치하게 되고 사용자에게 빈 입력을 방지하기 위해 다시 입력하도록 유도하는 역할을 함
+            */
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    }
+
+     //함수 onKeyDown은 사용자가 enter 키를 눌렀을 때 호출할 이벤트 핸들러
+    const onKeyDown = (e) => {
+        //e.keyCode에는 현재 사용자가 누른 키보드의 키가 숫자로 변환되어 저장되는데, 13은 enter키를 의미한다.
+        //즉, e.keyCode가 13이면 함수 onSubmit 호출
+        if(e.onKeyDown === 13){
+            onSubmit();
+        }
+    }
+
     return (
         <div className = "TodoEditor">
             <h4>새로운 Todo 작성하기✏️</h4>
             <div className = "editor_wrapper">
-                <input placeholder="새로운 Todo.." />
-                <button> 추가 </button>
+                <input 
+                    ref = {inputRef}
+                    value = {content}
+                    onChange={OnchangeContent}
+                    onKeyDown={onkeydown}
+                    placeholder="새로운 Todo.."
+                />
+                <button onClick={onSubmit}> 추가 </button>
             </div>
         </div>
     )
 }
 
 export default TodoEditor;
+
+/*
+기능 흐름
+1. 사용자가 새로운 할 일 입력
+2. TodoEditor 컴포넌트에 있는 추가 버튼 클릭
+3. TodoEditor 컴포넌트는 부모인 App에게 아이템 추가 이벤트가 발생했음을 알리고 사용자가 추가한 할 일 데이터 전달
+4. App 컴포넌트는 TodoEditor 컴포넌트에서 받은 데이터를 이용해 새 아이템이 추가된 배열을 만들고 State 변수 todo값 업데이트
+5. TodoEditor 컴포넌트는 자연스러운 사용자 경험을 위해 할 일 입력 폼을 초기화
+*/
