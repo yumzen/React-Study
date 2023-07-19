8b0f54a9260ace2ff9a235c3d7c3d731f0ab4fd2
yumzen <yeom.ji.eun35@gmail.com>
2023-07-19 20:48:35 +0900

    [Update] Next.js와 tailwindcss를 이용한 반응형 Navbar

---
+++ Nextjs-Tailwindcss-Navbar/src/app/page.js
@@ -0,0 +1,38 @@
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <section
+        id="about"
+        className="text-white h-screen bg-black  text-center flex"
+      >
+        <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold  ">
+          About
+        </h1>
+      </section>
+      <section id="blog" className=" text-white h-screen bg-purple-900 flex">
+        <h1 className="m-auto font-waterfall text-7xl text-white font-bold">
+          Blog
+        </h1>
+      </section>
+      <section
+        id="contact"
+        className="text-white h-screen bg-black  text-center flex"
+      >
+        <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold">
+          Contact{' '}
+        </h1>
+      </section>
+      <section
+        id="projects"
+        className=" text-white h-screen bg-purple-900 flex"
+      >
+        <h1 className="m-auto font-waterfall text-7xl text-white font-bold">
+          Projects
+        </h1>
+      </section>
+    </div>
+  );
+}
