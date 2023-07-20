7d7c9cf4af3799d827582fc187b248253d4cbe70
yumzen <yeom.ji.eun35@gmail.com>
2023-07-20 15:44:08 +0900

    [Update] Navbar Component

---
+++ Nextjs-Portfolio/pages/index.js
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import Head from "next/head"
+import Navbar from "@/Components/Navbar"
+
+export default function Home() {
+    return (
+    <div>
+        <Head>
+            <title>Front-End Developer</title>
+        </Head>
+        <Navbar/>
+    </div>
+    )
+}
